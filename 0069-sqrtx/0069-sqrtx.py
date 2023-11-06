4ce14f4fca87e11457da5e703d74dd15fea49aad
Peter Blockman <37528158+peterblockman@users.noreply.github.com>
2023-11-06 12:54:55 +0700

    Time: 49 ms (41.02%), Space: 16.2 MB (65.96%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l, r = 0, x + 1
+
+        while l < r:
+            m = l + (r - l) // 2
+            if m * m > x:
+                r = m 
+            else:
+                l = m + 1 
+
+        return l - 1
+        
